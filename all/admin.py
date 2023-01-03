554ed47cf173b07c1fea481da1e94b5ec3f08012
Ilyosbek07 <qarshiboyevilyos18@gmail.com>
2023-01-03 21:52:09 +0500

    'test_changes'

--- all/admin.py
+++ all/admin.py
@@ -5,3 +5,7 @@ from all.models import ContactModel, PortfolioModel, ReviewsModel
 admin.site.register(ContactModel)
 admin.site.register(PortfolioModel)
 admin.site.register(ReviewsModel)
+# admin.site.register(Savollar)
+# admin.site.register(Category)
+#
+#
--- all/models.py
+++ all/models.py
@@ -32,3 +32,49 @@ class ContactModel(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# alohida proect portfolioga aloqasi yo'q
+# class Users(models.Model):
+#     tg_id = models.BigIntegerField()
+#     full_name = models.CharField(max_length=255)
+#     username = models.CharField(max_length=255)
+#     first_name = models.CharField(max_length=255)
+#     daily_score = models.IntegerField()
+#
+#
+# class Category(models.Model):
+#     nomi = models.CharField(max_length=155)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return self.nomi
+#
+#
+# class Savollar(models.Model):
+#     category = models.ForeignKey(Category, related_name='all_category', on_delete=models.CASCADE)
+#     savol = models.CharField(max_length=255)
+#     manbaa = models.CharField(max_length=255)
+#     javob_1 = models.CharField(max_length=55)
+#     javob_2 = models.CharField(max_length=55)
+#     javob_3 = models.CharField(max_length=55, null=True, blank=True)
+#     javob_4 = models.CharField(max_length=55, null=True, blank=True)
+#     FRESHMAN = '1'
+#     SOPHOMORE = '2'
+#     JUNIOR = '3'
+#     SENIOR = '4'
+#     YEAR_IN_SCHOOL_CHOICES = [
+#         (FRESHMAN, '1'),
+#         (SOPHOMORE, '2'),
+#         (JUNIOR, '3'),
+#         (SENIOR, '4'),
+#     ]
+#     javobi = models.CharField(
+#         max_length=2,
+#         choices=YEAR_IN_SCHOOL_CHOICES,
+#         default=FRESHMAN,
+#     )
+#     image = models.ImageField(null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.savol
